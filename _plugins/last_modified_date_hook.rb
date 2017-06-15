8af9c492bb7a71c1dc1d0c4cd1833a302a1960fa
Michael Klishin <michael@clojurewerkz.org>
2017-06-15 04:57:47 +0300

    Rename plugin file

--- _plugins/last_modified_date_hook.rb
+++ _plugins/last_modified_date_hook.rb
@@ -1,7 +1,7 @@
 Jekyll::Hooks.register :pages, :pre_render do |page, payload|
 
   # get the current page last modified time
-  modification_time = File.mtime( page.path )
+  modification_time = File.mtime(page.path)
 
   # inject modification_time in page's datas.
   payload['page']['last-modified-date'] = modification_time
